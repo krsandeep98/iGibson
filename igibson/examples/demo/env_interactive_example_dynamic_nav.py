a9b329df2cd953c329594405cdb8bc2a47b54e24
Sandeep Kumar <krsandeep98@gmail.com>
2021-08-11 11:05:13 -0400

    new files for running the demo for multiple robots from one config file, and the dynamic nav task

--- igibson/examples/demo/env_interactive_example_dynamic_nav.py
+++ igibson/examples/demo/env_interactive_example_dynamic_nav.py
@@ -1,22 +1,25 @@
 import logging
 import os
 
+import sys
+logging.disable(sys.maxsize)
+
 import igibson
-from igibson.envs.igibson_env_sandeep import iGibsonEnv
+from igibson.envs.igibson_env import iGibsonEnv
 from igibson.render.profiler import Profiler
 
 
 def main():
-    config_filename = os.path.join(igibson.example_config_path, "fetch_turtlebot_room_rearrangement.yaml")
+    config_filename = os.path.join(igibson.example_config_path, "turtlebot_dynamic_nav.yaml")
     # config_filename = os.path.join(igibson.example_config_path, "turtlebot_point_nav.yaml")
     env = iGibsonEnv(config_file=config_filename, mode="gui")
     for j in range(1):
         env.reset()
-        for i in range(1000):
+        for i in range(100):
             with Profiler("Environment action step"):
                 action = env.action_space.sample()
-                action1 = env.action_space1.sample()
-                state, reward, done, info = env.step(action, action1)
+                # action1 = env.action_space1.sample()
+                state, reward, done, info = env.step(action)
                 if done:
                     logging.info("Episode finished after {} timesteps".format(i + 1))
                     break
